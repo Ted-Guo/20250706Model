51fc5a7159e35250e86707146646511f1e48aa1e
NanTing <qcsoul579@gmail.com>
2025-09-02 22:21:33 +0800

    ablation studt test

--- Multilingual_Chatbot_Arena/test2_ablation_study.py
+++ Multilingual_Chatbot_Arena/test2_ablation_study.py
@@ -54,23 +54,54 @@ df_train = pd.read_parquet("pre_datas_0822.parquet");
 
 #cross encoder
 # =============================================================================
-# 
+# cross_score_a:
 # AUC base    : [0.56060792 0.56621538 0.56283247 0.57204699 0.56756913] mean = 0.5658543753905377
 # AUC + test  : [0.56156176 0.56625022 0.56300508 0.57258942 0.56815189] mean = 0.5663116736536746
 # ΔAUC (+test - base) = 0.00045729826313680724
 # Paired t-test : TtestResult(statistic=np.float64(2.811679742589963), pvalue=np.float64(0.04823442387800346), df=np.int64(4))
 # Wilcoxon test : WilcoxonResult(statistic=np.float64(0.0), pvalue=np.float64(0.0625))
-# 
 # =============================================================================
+
+# =============================================================================
+#cross_score_b: 提升幅度小但顯著 
+# AUC base    : mean = 0.5653779251621962 ± 0.005212982133855345
+# AUC + test  : mean = 0.5664916147603901 ± 0.0052775111831613476
+# ΔAUC per fold = [0.00143147 0.00160854 0.00081192 0.00177503 0.00026883 0.00117964
+#  0.00063475 0.00141253 0.00118589 0.00100612 0.00094087 0.00146642
+#  0.00119037 0.00139412 0.00039884]
+# ΔAUC mean = 0.001113689598194112
+# Paired t-test : TtestResult(statistic=np.float64(9.862601600840522), pvalue=np.float64(1.1084557504882247e-07), df=np.int64(14))
+# Wilcoxon test : WilcoxonResult(statistic=np.float64(0.0), pvalue=np.float64(6.103515625e-05))
+# =============================================================================
+
+
+
 pairs_a = list(zip(df_train["prompt"], df_train["response_a"]));
 pairs_b = list(zip(df_train["prompt"], df_train["response_b"]));
 df_train = pre_funs.cross_encoder(df_train, pairs_a, pairs_b);
 df_no_q = df_train[~df_train['prompt'].str.contains(r"[?？]", na=False)];
 
 
-
 base_cols = [col for col in df_no_q.columns if col.startswith("emb_response_a") or col.startswith("emb_response_b") or col.startswith("emb_prompt")];
-test_cols = [col for col in df_no_q.columns if col.startswith("cross_score_a")];
+#test_cols = [col for col in df_no_q.columns if col.startswith("cross_score_a")];
+#test_cols = [col for col in df_no_q.columns if col.startswith("cross_score_b")];
+test_cols = [col for col in df_no_q.columns if col.startswith("diff")];
+
 
 validator_funs.ablation_study_test(df_no_q, base_cols, test_cols);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
